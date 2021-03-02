c36da2d2eb46d6da89ff912257609d8196dc030a
Thomas Van Doren <thomas.vandoren@gmail.com>
2021-03-02 08:54:33 -0800

    Updates to include advice about emails/slacks.

--- sentiment.py
+++ sentiment.py
@@ -37,7 +37,8 @@ def cli(text):
         color = 'white'
     click.echo(combined_text)
     click.echo('Sentiment: ', nl='')
-    click.secho(sentiment, fg=color, bold=True)
+    sentiment_styled = click.style(sentiment, fg=color, bold=True)
+    click.echo(sentiment_styled)
 
     chart_char = '▇'
     chart_width = 50
@@ -45,6 +46,27 @@ def cli(text):
     click.echo('negative: ' + click.style(chart_char * round(negative * chart_width), fg='red') + f' {negative:.2f}')
     click.echo('mixed   : ' + click.style(chart_char * round(mixed * chart_width), fg='yellow') + f' {mixed:.2f}')
     click.echo('neutral : ' + click.style(chart_char * round(neutral * chart_width), fg='white') + f' {neutral:.2f}')
+
+    word_count = len(combined_text.split())
+    _reminders(word_count, sentiment_styled)
+
+
+def _reminders(word_count: int, sentiment_styled: str):
+    click.echo()
+    click.echo('\tTips for communicating over slack and email:')
+
+    word_count_color = 'green'
+    if word_count < 10:
+        word_count_color = 'red'
+    elif word_count < 30:
+        word_count_color = 'yellow'
+
+    click.echo(click.style('\t\t1. Is my email/slack too brief?', bold=True) + click.style(f' Words: {word_count}', fg=word_count_color))
+    click.echo(click.style('\t\t2. Are there any typos?', bold=True))  # FIXME: check for typos 2021-03-01
+    click.echo(click.style('\t\t3. What tone am I projecting?', bold=True) + f' Sentiment is {sentiment_styled}')
+    click.echo(click.style('\t\t4. Would it be helpful to talk instead?', bold=True))
+    click.echo(f'\t\t\tProbably! Make sure to consider this especially if this thread has gone back and forth more than twice.')
+    click.echo()
 
 
 if __name__ == '__main__':
